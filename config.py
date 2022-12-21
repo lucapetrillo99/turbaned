56a4ecfbbdb7dc6c75e3ec66beb18b22c04cd409
Luca Petrillo <lucapetrillo99@gmail.com>
2022-12-21 20:49:58 +0100

    Updated the script that handles a start and end date entered for analysis
    - Added check on dates entered
    - Added config file

---
+++ config.py
@@ -0,0 +1,17 @@
+# SCRIPT INFO
+SCRIPT_USAGE = 'main.py [-h] [START_DATE] [END_DATE] [-d DAYS_AGO] [-m MONTHS_AGO]\n'
+SCRIPT_DESCRIPTION = 'You can enter a [START_DATE] of your choice and indicate an [END_DATE](optional). Otherwise you ' \
+                     'can enter the number of [-d DAYS_AGO] or [-m MONTHS_AGO] to be analyzed starting from today. '
+
+# SCRIPT DIRECTORIES
+DATA_PATH = 'data/'
+CVE_PATH = DATA_PATH + '/cve/'
+TWEET_PATH = DATA_PATH + 'tweets/'
+FILTERED_TWEET_PATH = DATA_PATH + 'filtered_tweets/'
+PROCESSED_CVE_PATH = DATA_PATH + 'processed/cve/'
+PROCESSED_TWEET_PATH = DATA_PATH + 'processed/tweet/'
+PROCESSED_TWEET_CVE_PATH = DATA_PATH + 'processed/tweets_cve/'
+MODEL_PATH = DATA_PATH + 'model/'
+RESULTS_PATH = DATA_PATH + 'results/'
+
+DATE_FORMAT = '%d-%m-%Y'
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import config
 import argparse
 import analysis
 
@@ -6,48 +7,111 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-def check_valid_argument(argument):
-    if argument.daysAgo < 0 and argument.monthsAgo < 0:
+def check_valid_arguments(argument):
+    if argument.days_ago < 0 and argument.months_ago < 0:
         print("ERROR! days and months have to be positive, exiting...")
         return False
-    elif argument.daysAgo < 0:
+    elif argument.days_ago < 0:
         print("ERROR! days have to be positive, exiting...")
         return False
-    elif argument.monthsAgo < 0:
+    elif argument.months_ago < 0:
         print("ERROR! months have to be positive, exiting...")
         return False
     else:
         return True
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--daysAgo", type=int, default=0, help="Analyzed CVE of X days ago")
-    parser.add_argument("-m", "--monthsAgo", type=int, default=0, help="And analyzed CVE of X months ago")
-    args = parser.parse_args()
+def check_valid_dates(inserted_dates):
+    is_valid = True
+    valid_dates = []
+    if len(inserted_dates) > 2:
+        is_valid = False
+    elif len(inserted_dates) == 1:
+        try:
+            valid_dates.append(datetime.strptime(inserted_dates[0], config.DATE_FORMAT))
+        except ValueError:
+            print("Incorrect data format, should be YYYY-MM-DD, exiting...")
+            is_valid = False
+    else:
+        for date in inserted_dates:
+            try:
+                valid_dates.append(datetime.strptime(date, config.DATE_FORMAT))
+            except ValueError:
+                print("Incorrect data format, should be YYYY-MM-DD, exiting...")
+                is_valid = False
 
-    if args is not None and check_valid_argument(args):
-        current_date = datetime.now()
-        current_date = datetime(year=current_date.year, month=current_date.month, day=current_date.day)
-        if args.daysAgo == 0 and args.monthsAgo == 0:
+    if is_valid:
+        return valid_dates
+    else:
+        return False
+
+
+def start_script(start, end):
+    if not (os.path.exists('data')):
+        try:
+            os.mkdir(config.DATA_PATH)
+            os.mkdir(config.CVE_PATH)
+            os.mkdir(config.TWEET_PATH)
+            os.mkdir(config.FILTERED_TWEET_PATH)
+            os.makedirs(config.PROCESSED_CVE_PATH)
+            os.makedirs(config.PROCESSED_TWEET_PATH)
+            os.makedirs(config.PROCESSED_TWEET_CVE_PATH)
+            os.mkdir(config.MODEL_PATH)
+            os.mkdir(config.RESULTS_PATH)
+        except FileExistsError:
+            pass
+
+    if end is None:
+        end = current_date
+
+    print(
+        "Start analysis from: {0} to {1}".format(start.strftime(config.DATE_FORMAT), end.strftime(config.DATE_FORMAT)))
+    analysis.start_analysis(start_date)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(usage=config.SCRIPT_USAGE,
+                                     description=config.SCRIPT_DESCRIPTION)
+    parser.add_argument("-d", "--days-ago", required=False, type=int, default=0, help="Analyzed tweets of X days ago")
+    parser.add_argument("-m", "--months-ago", required=False, type=int, default=0,
+                        help="Analyzed tweets of X months ago")
+    args, dates = parser.parse_known_args()
+    start_date = None
+    end_date = None
+    current_date = datetime.now()
+
+    if dates and (args.days_ago != 0 or args.months_ago != 0):
+        print("Too many arguments, exiting...")
+        exit(0)
+    elif dates:
+        values = check_valid_dates(inserted_dates=dates)
+        if values:
+            if len(values) > 1:
+                if values[0] > current_date or values[1] > current_date:
+                    print("Wrong date, exiting...")
+                    exit(0)
+                elif values[0] > values[1]:
+                    print("Wrong date order, exiting...")
+                    exit(0)
+                else:
+                    start_date = values[0]
+                    end_date = values[1]
+            else:
+                if values[0] > current_date:
+                    print("Wrong date, exiting...")
+                    exit(0)
+                else:
+                    start_date = values[0]
+                    end_date = current_date
+            start_script(start_date, end_date)
+        else:
+            exit(0)
+    elif check_valid_arguments(args):
+        if args.days_ago == 0 and args.months_ago == 0:
             start_date = current_date
         else:
-            start_date = (current_date - relativedelta(days=args.daysAgo, months=args.monthsAgo,
+            start_date = (current_date - relativedelta(days=args.days_ago, months=args.months_ago,
                                                        year=current_date.year))
-        if not (os.path.exists('data')):
-            try:
-                os.mkdir('data')
-                os.mkdir('data/cve')
-                os.mkdir('data/tweets')
-                os.mkdir('data/filtered_tweets')
-                os.makedirs('data/processed/tweet')
-                os.makedirs('data/processed/tweets_cve')
-                os.mkdir('data/models')
-                os.mkdir('data/results')
-            except FileExistsError:
-                pass
-
-        print("Start analysis from: {}".format(start_date.strftime("%d-%m-%Y")))
-        analysis.start_analysis(start_date)
+        start_script(start_date, end_date)
     else:
         exit(0)
