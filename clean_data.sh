80bb900f654e3206f3102ab3e2174dae9bb7ddb7
Luca Petrillo <lucapetrillo99@gmail.com>
2022-12-28 17:18:31 +0100

    Refactored script to clean data

--- clean_data.sh
+++ clean_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TWEET_PATH='data/tweets/'
+FILTERED_TWEET_PATH='data/filtered_tweets/'
 CVE_PATH='data/cve/'
 
-if [ -n "$(ls -A "$TWEET_PATH")" ]; then
-  for file in "$TWEET_PATH"*; do
+if [ -n "$(ls -A "$FILTERED_TWEET_PATH")" ]; then
+  for file in "$FILTERED_TWEET_PATH"*; do
     rm "$file"
   done
 fi
