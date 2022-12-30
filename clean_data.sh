45fe0479a4ff22363c407a9e49da56cb5d008256
Luca Petrillo <lucapetrillo99@gmail.com>
2022-12-30 12:54:38 +0100

    Refactored script to delete data

--- clean_data.sh
+++ clean_data.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 FILTERED_TWEET_PATH='data/filtered_tweets/'
+CVE_REFERENCES_PATH='data/cve_references/'
 CVE_PATH='data/cve/'
 
 if [ -n "$(ls -A "$FILTERED_TWEET_PATH")" ]; then
   for file in "$FILTERED_TWEET_PATH"*; do
+    rm "$file"
+  done
+fi
+
+
+if [ -n "$(ls -A "$CVE_REFERENCES_PATH")" ]; then
+  for file in "$CVE_REFERENCES_PATH"*; do
     rm "$file"
   done
 fi
